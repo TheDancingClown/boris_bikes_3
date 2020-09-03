563118d1e50fee407cbf2c05a1e0b79d636c1ed8
Louis Kirkham <louiskirkham@gmail.com>
2020-09-03 15:32:03 +0100

    added exercise 10

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,5 +4,14 @@ describe DockingStation do
     it 'responds to releasing a bike from docking station' do
       expect(DockingStation.new).to respond_to(:release_bike) #DockingStation.new is the object of the class
     end
+
+    it 'creates a new instance of Bike' do
+      expect((DockingStation.new).release_bike).to respond_to(:working?)
+    end
+
+    it 'expects true when working is called on a Bike' do
+      expect(subject.release_bike.working?).to eq(true)
+    end
+    
   end
 end
